34555540b5239774d796d107c69afcb7b6d608fd
Kamsiyochukwu-Orji <kamsiyochukwuorji@yahoo.com>
2026-06-04 21:12:04 +0100

    chore:initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const display = document.getElementById("display");
+const historyList = document.getElementById("historyList");
+const themeToggle = document.getElementById("themeToggle");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function appendFunction(func) {
+  display.value += func;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    let expression = display.value;
+
+    const result = eval(expression);
+
+    const item = document.createElement("li");
+    item.textContent = `${expression} = ${result}`;
+
+    historyList.prepend(item);
+
+    display.value = result;
+  } catch {
+    display.value = "Error";
+  }
+}
+
+document.addEventListener("keydown", (e) => {
+  if (!isNaN(e.key) || "+-*/.%".includes(e.key)) {
+    display.value += e.key;
+  }
+
+  if (e.key === "Enter") {
+    calculate();
+  }
+
+  if (e.key === "Backspace") {
+    deleteLast();
+  }
+
+  if (e.key === "Escape") {
+    clearDisplay();
+  }
+});
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+
+  themeToggle.textContent = document.body.classList.contains("light-mode")
+    ? "☀️"
+    : "🌙";
+});
